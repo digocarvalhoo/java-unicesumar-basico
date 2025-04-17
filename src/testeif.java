9b2860ccff995bad6cff8df9723b94bac6289012
digocarvalhoo <diogocarva25@gmail.com>
2025-04-17 16:33:37 -0300

    update name

---
+++ src/testeif.java
@@ -0,0 +1,12 @@
+public class testeif {
+    public static void main(String[] args) {
+        
+        int valor1 = 5;
+        int valor2 = 10;
+
+        if (valor1 > valor2) {
+            System.out.println("Valor 1 é maior");
+        }
+        System.out.println("Soma dos valores: " + (valor1 + valor2));
+    }
+}
